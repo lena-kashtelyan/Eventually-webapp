915e0fda90d6d15dc08198f0845c3a924a3109b2
Will Groves <william_groves@brown.edu>
2016-04-18 19:00:00 -0400

    login classes

--- src/main/java/edu/brown/cs/finalproject/credentials/ForgotPassword.java
+++ src/main/java/edu/brown/cs/finalproject/credentials/ForgotPassword.java
@@ -1,5 +1,74 @@
 package edu.brown.cs.finalproject.credentials;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ForgotPassword {
+
+  public static boolean checkUserName(String username, Connection conn) {
+    String query = "SELECT COUNT(username) FROM users WHERE username=?";
+    try (PreparedStatement prep = conn.prepareStatement(query)) {
+      prep.setString(1, username);
+      try (ResultSet rs = prep.executeQuery()) {
+        int count = rs.getInt(1);
+        if (count != 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } catch (NullPointerException | SQLException n) {
+      n.printStackTrace();
+      return false;
+    }
+  }
+
+  public static String[] getQuestions(String username, Connection conn) {
+    if (checkUserName(username, conn)) {
+      String query = "SELECT q1,q2 FROM users WHERE username=?";
+      try (PreparedStatement prep = conn.prepareStatement(query)) {
+        prep.setString(1, username);
+        try (ResultSet rs = prep.executeQuery()) {
+          String q1 = rs.getString(1);
+          String q2 = rs.getString(2);
+          String[] answers = new String[] { q1, q2 };
+          return answers;
+        }
+      } catch (NullPointerException | SQLException n) {
+        n.printStackTrace();
+        return null;
+      }
+    } else {
+      System.out.println("username not in database");
+      return null;
+    }
+  }
+
+  public static boolean checkAnswers(String[] answers, String username,
+      Connection conn) {
+    if (checkUserName(username, conn)) {
+      String query = "SELECT a1,a2 FROM users WHERE username=?";
+      try (PreparedStatement prep = conn.prepareStatement(query)) {
+        prep.setString(1, username);
+        try (ResultSet rs = prep.executeQuery()) {
+          String a1 = rs.getString(1);
+          String a2 = rs.getString(2);
+          if (answers[0].equals(a1) && answers[1].equals(a2)) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      } catch (NullPointerException | SQLException n) {
+        n.printStackTrace();
+        return false;
+      }
+    } else {
+      System.out.println("username not in database");
+      return false;
+    }
+  }
 
 }
--- src/main/java/edu/brown/cs/finalproject/credentials/SignUp.java
+++ src/main/java/edu/brown/cs/finalproject/credentials/SignUp.java
@@ -1,5 +1,61 @@
 package edu.brown.cs.finalproject.credentials;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class SignUp {
+
+  public static boolean isUnique(String username, Connection conn) {
+    String query = "SELECT COUNT(username) FROM users WHERE username=?";
+    try (PreparedStatement prep = conn.prepareStatement(query)) {
+      prep.setString(1, username);
+      try (ResultSet rs = prep.executeQuery()) {
+        int count = rs.getInt(1);
+        if (count == 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } catch (NullPointerException | SQLException n) {
+      n.printStackTrace();
+      return false;
+    }
+  }
+
+  /**
+   * @param fields
+   *          - array of fields for new user ordered by the SQL table
+   * @param conn
+   *          - connection to database
+   * @return true if user was added, false if not
+   */
+  public static boolean addUser(String[] fields, Connection conn) {
+    if (fields.length != 8) {
+      System.out.println("fields must contain exactly 8 values");
+      return false;
+    }
+
+    if (isUnique(fields[0], conn)) {
+      String query = "INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?)";
+      try (PreparedStatement prep = conn.prepareStatement(query)) {
+        prep.setString(1, UUID.randomUUID().toString());
+        for (int i = 0; i < fields.length; i++) {
+          prep.setString(i + 2, fields[i]);
+        }
+        prep.addBatch();
+        prep.executeBatch();
+        return true;
+      } catch (NullPointerException | SQLException n) {
+        n.printStackTrace();
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
 
 }
