9d54e28ca20bf23ed5a0a2e49b286ab193cc4801
Will Groves <william_groves@brown.edu>
2016-04-19 21:19:16 -0400

    blah

--- src/main/java/edu/brown/cs/finalproject/main/Main.java
+++ src/main/java/edu/brown/cs/finalproject/main/Main.java
@@ -1,27 +1,25 @@
 package edu.brown.cs.finalproject.main;
 
 public class Main {
-	public static void main(String[] args) {
-		new Main(args).run();
-	}
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
 
-	private String[] args;
+  private String[] args;
 
-	private Main(String[] args) {
-		this.args = args;
-	}
+  private Main(String[] args) {
+    this.args = args;
+  }
 
-	private void run() {
-		System.out.println("helloworld");
+  private void run() {
+    System.out.println("helloworld");
 
-//		lines to instantiate tables in the database and create indices; uncomment and change login name if you want to reset the tables
-//		Database db = new Database("/home/ipetrov/course/cs032/final_project/32final/database/finalproject.db");
-//		new DatabaseFactory(db).createAndIndexTables();	
-		
-		
-		
-		
-		
-		System.out.println("all done");
-	}	
+    // lines to instantiate tables in the database and create indices; uncomment
+    // and change login name if you want to reset the tables
+    // Database db = new
+    // Database("/home/ipetrov/course/cs032/final_project/32final/database/finalproject.db");
+    // new DatabaseFactory(db).createAndIndexTables();
+
+    System.out.println("all done");
+  }
 }
