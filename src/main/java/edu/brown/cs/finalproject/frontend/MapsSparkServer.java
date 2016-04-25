a073761b218acacea652d3785071755e570d35e9
Cole Hansen <cole_hansen@brown.edu>
2016-04-24 21:37:05 -0400

    finished signup

--- src/main/java/edu/brown/cs/finalproject/frontend/MapsSparkServer.java
+++ src/main/java/edu/brown/cs/finalproject/frontend/MapsSparkServer.java
@@ -24,10 +24,8 @@ public class MapsSparkServer implements SparkServer {
     Spark.get("/map", new MapView(), freeMarker);
     Spark.get("/login", new LoginView("login.ftl"), freeMarker);
     Spark.post("/login", new LoginHandler());
-    // Spark.get("/forgot", new ForgotView("forgot.ftl"),
-    // freeMarker);
-    // Spark.post("/forgot", new ForgotHandler(),
-    // freeMarker);
+    Spark.get("/forgot", new ForgotView("forgot.ftl"), freeMarker);
+    Spark.post("/forgot", new ForgotHandler());
     Spark.get("/signup", new SignupView("signup.ftl"), freeMarker);
     Spark.post("/signup", new SignupHandler());
     // Spark.get("/map", new MapView(), freeMarker);
