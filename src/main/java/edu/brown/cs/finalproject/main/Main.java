6dce2975784e3d0c630f25dcdfb63faf82ba0b10
Cole Hansen <cole_hansen@brown.edu>
2016-04-26 21:46:35 -0400

    Working on FB login

--- src/main/java/edu/brown/cs/finalproject/main/Main.java
+++ src/main/java/edu/brown/cs/finalproject/main/Main.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import com.google.gson.JsonObject;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.FacebookClient.AccessToken;
-import com.restfb.Version;
-import com.restfb.scope.ScopeBuilder;
+import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.directory.CreateDirectoryRequest;
+import com.stormpath.sdk.directory.Directories;
+import com.stormpath.sdk.directory.Directory;
+import com.stormpath.sdk.provider.ProviderAccountRequest;
+import com.stormpath.sdk.provider.ProviderAccountResult;
+import com.stormpath.sdk.provider.Providers;
+import com.stormpath.sdk.tenant.Tenant;
 
 import edu.brown.cs.finalproject.credentials.Authenticator;
 import edu.brown.cs.finalproject.credentials.Login;
@@ -17,13 +20,9 @@ import edu.brown.cs.finalproject.credentials.StormPathApplication;
 import edu.brown.cs.finalproject.database.Database;
 import edu.brown.cs.finalproject.database.DatabaseManager;
 import edu.brown.cs.finalproject.database.PublicFBEventsWriter;
-import edu.brown.cs.finalproject.frontend.MapsSparkServer;
-import edu.brown.cs.finalproject.frontend.SparkServer;
-import edu.brown.cs.finalproject.search.FacebookDataManager;
 import edu.brown.cs.finalproject.frontend.BackendInteraction;
 import edu.brown.cs.finalproject.frontend.MapsSparkServer;
 import edu.brown.cs.finalproject.frontend.SparkServer;
-import edu.brown.cs.finalproject.search.EventsByName;
 import edu.brown.cs.finalproject.search.FacebookDataManager;
 import edu.brown.cs.finalproject.search.PublicFBEventsFinder;
 import joptsimple.OptionParser;
@@ -53,6 +52,39 @@ public class Main {
         "cole_hansen@brown.edu", "P@ssword1");
 
     try {
+      Directory directory = stormPathApp.getStormPathClient()
+          .instantiate(Directory.class);
+      directory.setName("test-facebook-directory");
+      directory.setDescription("Test Facebook directory");
+
+      CreateDirectoryRequest request = Directories
+          .newCreateRequestFor(directory)
+          .forProvider(
+              Providers.FACEBOOK.builder().setClientId("220099498366885")
+                  .setClientSecret("8a0e23ef1bc9e94213c881e53b2d7343").build())
+          .build();
+
+      Tenant tenant = stormPathApp.getStormPathClient().getCurrentTenant();
+      directory = tenant.createDirectory(request);
+    } catch (Exception e) {
+    }
+
+    try {
+      String applicationHref = "https://api.stormpath.com/v1/applications/76713eIdUzokAFDoD4WtP7";
+      Application application = stormPathApp.getStormPathClient()
+          .getResource(applicationHref, Application.class);
+      ProviderAccountRequest request = Providers.FACEBOOK.account()
+          .setAccessToken(
+              "CABTmZxAZBxBADbr1l7ZCwHpjivBt9T0GZBqjQdTmgyO0OkUq37HYaBi4F23f49f5")
+          .build();
+
+      ProviderAccountResult result = application.getAccount(request);
+      Account account = result.getAccount();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    try {
       auth.createAccount(test);
     } catch (RuntimeException e) {
       System.out.println(e.getMessage());
@@ -78,7 +110,6 @@ public class Main {
     // new DatabaseFactory().createAndIndexTables();
     System.out.println("all done");
 
-
     if (options.has("gui")) {
       new BackendInteraction(auth, dbManager, facebookDataManager);
       SparkServer server = new MapsSparkServer();
@@ -91,34 +122,33 @@ public class Main {
       // THIS IS HOW WE FETCH PUBLIC FACEBOOK EVENTS AND
       // UPDATE CARTODB events TABLE
 
-			try {
-				new PublicFBEventsFinder();
-			} catch (Exception e1) {
-				e1.printStackTrace();
-				System.out
-						.println("ERROR: Problem with running the public events application.");
-			}
+      try {
+        new PublicFBEventsFinder();
+      } catch (Exception e1) {
+        e1.printStackTrace();
+        System.out.println(
+            "ERROR: Problem with running the public events application.");
+      }
 
-			JsonObject publicEvents = null;
-			try {
-				publicEvents = PublicFBEventsFinder.requestEvents(41.826119,
-						-71.403112, 1000);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-				System.out.println("ERROR: Fetching public Facebook events.");
-			}
+      JsonObject publicEvents = null;
+      try {
+        publicEvents = PublicFBEventsFinder.requestEvents(41.826119, -71.403112,
+            1000);
+      } catch (IOException e1) {
+        e1.printStackTrace();
+        System.out.println("ERROR: Fetching public Facebook events.");
+      }
 
-			System.out.println(publicEvents);
-			PublicFBEventsWriter publicFBEventsWriter = new PublicFBEventsWriter();
-			try {
-				publicFBEventsWriter.updateDB(publicEvents);
-			} catch (SQLException | IOException e) {
-				e.printStackTrace();
-				System.out
-						.println("Problem updating database with public venues.");
-			}
-      
-//       EventsByName eventsByName = new EventsByName();
+      System.out.println(publicEvents);
+      PublicFBEventsWriter publicFBEventsWriter = new PublicFBEventsWriter();
+      try {
+        publicFBEventsWriter.updateDB(publicEvents);
+      } catch (SQLException | IOException e) {
+        e.printStackTrace();
+        System.out.println("Problem updating database with public venues.");
+      }
+
+      // EventsByName eventsByName = new EventsByName();
 
     }
 
