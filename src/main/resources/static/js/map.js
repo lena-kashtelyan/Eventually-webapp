90a2ba7e0643c347c31a346959313555998a08c3
Cole Hansen <cole_hansen@brown.edu>
2016-05-04 19:55:28 -0400

    Working on adding map

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,16 +1,4 @@
-$(document).ready(function() {
-  console.log("pront");
-  var map = new L.Map('map-container', {
-    center: [0,0],
-    zoom: 2
-  });
-
-  cartodb.createLayer(map, 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json')
-    .addTo(map)
-    .on('done', function(layer) {
-      console.log("test");
-    })
-    .on('error', function(err) {
-      alert("some error occurred: " + err);
-    });
-});
+window.onload = function() {
+  var vizjson = 'https://cs32finalproject.cartodb.com/api/v2/viz/ea773bf6-124e-11e6-942b-0e787de82d45/viz.json';
+  cartodb.createVis('map-container', vizjson);
+}
