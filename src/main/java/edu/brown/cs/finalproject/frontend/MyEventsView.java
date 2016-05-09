63fdafa6815f79d64802bae6a280883b57f0ccc9
Lena Kashtelyan <elena_kashtelyan@brown.edu>
2016-05-09 01:52:10 -0400

    resolved merge conflicts

--- src/main/java/edu/brown/cs/finalproject/frontend/MyEventsView.java
+++ src/main/java/edu/brown/cs/finalproject/frontend/MyEventsView.java
@@ -14,8 +14,6 @@ import spark.QueryParamsMap;
 import spark.Request;
 import spark.Response;
 import spark.TemplateViewRoute;
-
-<<<<<<< HEAD
 import com.google.common.collect.ImmutableMap;
 
 import edu.brown.cs.finalproject.credentials.AuthToken;
@@ -26,10 +24,7 @@ import edu.brown.cs.finalproject.entities.EventProxy;
 import edu.brown.cs.finalproject.entities.Media;
 import edu.brown.cs.finalproject.entities.User;
 
-=======
->>>>>>> 4e69b87d66ca23b594619c35f78449118cf27ab2
-/**
- * Private visible class to handle the serving of the login
+ /* Private visible class to handle the serving of the login
  * ftl template.
  */
 public class MyEventsView extends BackendInteraction
@@ -61,7 +56,6 @@ public class MyEventsView extends BackendInteraction
     if (authString != null) {
       AuthToken authToken = AuthToken.generateAuthToken(authString);
       if (auth.verifyAuthToken(username, authToken)) {
-<<<<<<< HEAD
           BrowseResultsHolder browseResults = DatabaseManager.getUpcomingEventsWithinProximitySortedByPopularity(41.826144690402, -71.403125740801, 10000, 100, username);
 
           HashMap<String, Boolean> userSavedEvents = browseResults.getUserSavedEvents();
@@ -72,48 +66,21 @@ public class MyEventsView extends BackendInteraction
           List<Event> past = new ArrayList<Event>();
           //list of suggested events, to be worked on later
           List<Event> suggested = new ArrayList<Event>();
-        //try {
-          //fill the lists
-=======
-        // list of events marked "saved" and "attending" in
-        // chronological order (most immediate on top)
-        List<Event> upcoming = new ArrayList<Event>();
-        // list of past events marked "saved" and
-        // "attending" in reverse-chronological order (most
-        // recent on top)
-        List<Event> past = new ArrayList<Event>();
-        // list of suggested events, to be worked on later
-        List<Event> suggested = new ArrayList<Event>();
-        try {
+ 
+ //       try {
           // fill the lists
-
->>>>>>> 4e69b87d66ca23b594619c35f78449118cf27ab2
           Map<Object, Object> data = ImmutableMap.builder()
               .put("title", "My Events").put("upcoming", upcoming)
               .put("past", past).put("suggested", suggested)
-              .put("username", username).put("auth", authToken.toString())
-<<<<<<< HEAD
-              .put("userSavedEvents", userSavedEvents)
-              .put("userAttendingEvents", userAttendingEvents).build();
+              .put("username", username).put("auth", authToken.toString()).build();
           return new ModelAndView(data, htmlUrl);
-//        } catch (ClassNotFoundException e) {
+//        } catch (Exception e) {
 //          // TODO Auto-generated catch block
 //          e.printStackTrace();
 //          Map<Object, Object> data = ImmutableMap.builder()
 //              .put("title", "Ooops").build();
 //          return new ModelAndView(data, "oops.ftl");
 //        }
-=======
-              .build();
-          return new ModelAndView(data, htmlUrl);
-        } catch (Exception e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-          Map<Object, Object> data = ImmutableMap.builder()
-              .put("title", "Ooops").build();
-          return new ModelAndView(data, "oops.ftl");
-        }
->>>>>>> 4e69b87d66ca23b594619c35f78449118cf27ab2
       } else {
         // malicious user, redirect to login
         Map<Object, Object> data = ImmutableMap.builder().put("title", "Login")
